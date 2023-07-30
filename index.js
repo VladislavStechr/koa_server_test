00e249b60e225182cf19d545951f9cf1039dd03d
Vladislav Stechr <vladislav.stechr@socialbakers.com>
2023-07-30 18:10:18 +0200

    added POST message and GET stats with basic functionality

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const PORT = 3000
 const Koa = require('koa')
 const Router = require('@koa/router')
+const { bodyParser } = require("@koa/bodyparser")
+
+const messageStats = {
+	count: 0,
+	latestMessage: null
+}
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/', (ctx) => {
-	ctx.body = 'Hello Koa'
+router.post('/message', (ctx) => {
+	messageStats.latestMessage = ctx.request.body
+	++messageStats.count
+
+	ctx.status = 200;
+	ctx.body = 'OK'
 })
 
+router.get('/stats', (ctx) => {
+	ctx.body = messageStats
+})
+
+app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
